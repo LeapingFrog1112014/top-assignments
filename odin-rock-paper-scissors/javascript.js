f6cfd29b9d7cb77fde2f0a72a28e88753ea80b08
Dilan C. Tern <127003284+dct-98@users.noreply.github.com>
2023-04-30 18:06:46 -0700

    Completed playRound function announce function

--- odin-rock-paper-scissors/javascript.js
+++ odin-rock-paper-scissors/javascript.js
@@ -11,19 +11,51 @@ function getComputerChoice () {
     }
 }
 
+function announce (result, playerSelection, computerSelection) {
+    if (result[0] == false) {
+        if (result[1] == false) {
+            return(playerSelection + " is equal to " + computerSelection + ". Draw.")
+        }
+        else {
+            return(playerSelection + " loses to " + computerSelection+ ". You lose.")
+        }
+    }
+    else {
+        return(playerSelection + " beats " + computerSelection + ". You win!")
+    }
+}
+
 function playRound (playerSelection, computerSelection) {
+    let result = undefined
     if (playerSelection == computerSelection) {
-        return [false, false]
+        result = [false, false] // Draw case
     }
     else if (playerSelection == "rock") {
-
+        if (computerSelection == "paper") {
+            result = [false, true]
+        }
+        else {
+            result = [true, false]
+        }
     }
 
     else if (playerSelection == "paper") {
-
+        if (computerSelection == "rock") {
+            result = [true , false]
+        }
+        else {
+            result = [false, true]
+        }
     }
 
     else {
-        
+        if (computerSelection == "rock") {
+            result = [false , true]
+        }
+        else {
+            result = [true, false]
+        }
     }
+    console.log(announce(result, playerSelection, computerSelection))
+    return result
 }

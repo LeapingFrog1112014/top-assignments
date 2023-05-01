d725882cdbd61a6cd1952d88f6f8e9e7c63371cb
Dilan C. Tern <127003284+dct-98@users.noreply.github.com>
2023-04-30 22:01:08 -0700

    Completed iterate function

--- javascript-practice/fizzBuzz/javascript.js
+++ javascript-practice/fizzBuzz/javascript.js
@@ -1,7 +1,14 @@
-function fizzBuzz (num, str) {
-
+function fizzBuzz (num) {
+    
 }
 
 function iterate (num) {
-    
+    for (let i = 1; i >= num; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            console.log(fizzBuzz(i));
+        }
+        else {
+            console.log(i)
+        }
+    }    
 }
